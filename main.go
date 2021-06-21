2b764db71cf7793af1bbb93928ff01152440f1f9
aspedrosa <aspedrosa@ua.pt>
2021-06-21 15:16:04 +0100

    add start and stop actions

--- main.go
+++ main.go
@@ -13,33 +13,35 @@ type Pipeline struct {
   Name string  `gorm:"not null;unique"`
   Selections []PipelineSelection
   Filter string
-  Status string  `gorm:"not null"`
+  Status string  `gorm:"not null;default:'STOPPED'"`
 }
 
 type PipelineSelection struct {
   gorm.Model
-  PipelineId uint
+  PipelineID uint `gorm:"uniqueIndex:unique_column"`
   Column string `gorm:"not null;uniqueIndex:unique_column"`
   Order int `gorm:"default:0"`
 }
 
 type Subscription struct {
   gorm.Model
-  Pipeline Pipeline
+  PipelineID uint
   Name string  `gorm:"not null;unique"`
   Requests []Request
-  Status string  `gorm:"not null"`
+  Status string  `gorm:"not null;default:'STOPPED'"`
 }
 
 type Request struct {
   gorm.Model
-  SubscriptionId uint
+  SubscriptionID uint
   Request_arguments_template string  `gorm:"not null"`
   Success_condition_template string  `gorm:"not null"`
   Order int  `gorm:"default:0"`
 }
 
 func main() {
+  //os.Remove("demo.db")
+
   DB, _ := gorm.Open("sqlite3", "demo.db")
   DB.AutoMigrate(&Pipeline{}, &PipelineSelection{}, &Subscription{}, &Request{})
 
@@ -49,12 +51,41 @@ func main() {
   // Allow to use Admin to manage Pipeline and Subscriptions
   pipeline := Admin.AddResource(&Pipeline{})
   pipeline.NewAttrs("-Status")
+  pipeline.EditAttrs("-Status")
+
+  selection := pipeline.Meta(&admin.Meta{Name: "Selections"}).Resource
+  selection.Meta(&admin.Meta{
+    Name: "Column",
+    Config: &admin.SelectOneConfig{
+      Collection: []string{
+        "analysis_id",
+        "stratum_1",
+        "stratum_2",
+        "stratum_3",
+        "stratum_4",
+        "stratum_5",
+        "count_value",
+        "min_value",
+        "max_value",
+        "avg_value",
+        "stdev_value",
+        "mean_value",
+        "p10_value",
+        "p25_value",
+        "p75_value",
+        "p90_value",
+      },
+    },
+  })
 
   sub := Admin.AddResource(&Subscription{})
+
   sub.NewAttrs("-Status")
+  sub.EditAttrs("-Status")
+
+  // select between existing pipelines
   sub.Meta(&admin.Meta{
-    Name: "Pipeline",
-    Label: "Pipeline",
+    Name: "PipelineID",
     Config: &admin.SelectOneConfig{
       Collection: func(_ interface{}, context *admin.Context) (options [][]string) {
         var pipelines []Pipeline
@@ -70,8 +101,96 @@ func main() {
       },
     },
   })
+  // start subscription action
+  sub.Action(&admin.Action{
+    Name: "Start",
+    Handler: func(actionArgument *admin.ActionArgument) error {
+      db := actionArgument.Context.GetDB()
+      for _, record := range actionArgument.FindSelectedRecords() {
+        subscription := record.(*Subscription)
+
+        if subscription.Status == "ACTIVE" {
+          continue
+        }
+
+        err := db.Transaction(func(tx *gorm.DB) error {
+          subscription.Status = "ACTIVE"
+          tx.Save(&subscription)
+
+          var pipeline Pipeline
+          tx.Take(&pipeline, record.(*Subscription).PipelineID)
+
+          if pipeline.Status == "STOPPED" {
+            pipeline.Status = "ACTIVE"
+          	tx.Save(&pipeline)
+          }
+
+          return nil
+        })
+
+        if err != nil {
+          return err
+        }
+      }
+      return nil
+    },
+    Visible: func(record interface{}, context *admin.Context) bool {
+      if record == nil {
+        return true
+      }
+      return record.(*Subscription).Status == "STOPPED"
+    },
+    Modes: []string{"edit", "show", "menu_item", "batch"},
+  })
+  sub.Action(&admin.Action{
+    Name: "Stop",
+    Handler: func(actionArgument *admin.ActionArgument) error {
+      db := actionArgument.Context.GetDB()
+      for _, record := range actionArgument.FindSelectedRecords() {
+        subscription := record.(*Subscription)
+
+        if subscription.Status == "STOPPED" {
+          continue
+        }
+
+        err := db.Transaction(func(tx *gorm.DB) error {
+          subscription.Status = "STOPPED"
+          tx.Save(&subscription)
+
+          var count uint
+          tx2 := tx.Model(&Subscription{}).Where("pipeline_id = ? AND status = ?", subscription.PipelineID, "ACTIVE").Count(&count)
+          fmt.Println(tx2.Error)
+
+          if count == 0 {
+            var pipeline Pipeline
+          	tx.Take(&pipeline, subscription.PipelineID)
+
+            pipeline.Status = "STOPPED"
+            tx.Save(&pipeline)
+          }
+
+          return nil
+        })
+
+        if err != nil {
+          return err
+        }
+      }
+
+      return nil
+    },
+    Visible: func(record interface{}, context *admin.Context) bool {
+      if record == nil {
+        return true
+      }
+      return record.(*Subscription).Status == "ACTIVE"
+    },
+    Modes: []string{"edit", "show", "menu_item", "batch"},
+  })
+
 
   request := sub.Meta(&admin.Meta{Name: "Requests"}).Resource
+  // change some inputs to textarea
   request.Meta(&admin.Meta{
     Name: "Request_arguments_template",
     Type: "text",
