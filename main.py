c208706b9de42d553ce0ff43e5d79f03c94b4e25
aspedrosa <aspedrosa@ua.pt>
2021-06-22 13:00:05 +0100

    finalize main structure of receiving end

--- main.py
+++ main.py
@@ -1,54 +1,72 @@
 import asyncio
 
-from aiokafka import AIOKafkaConsumer
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 from kafka import TopicPartition
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from task import send_updates
 
 
 async def main():
+    db_engine = create_async_engine("sqlite:///demo.db")
+
     consumer = AIOKafkaConsumer(
-        'my_topic', 'my_other_topic',
-        bootstrap_servers='localhost:9092',
-        group_id="my-group",
+        "PIPELINES_UPLOAD_NOTIFICATIONS",
+        bootstrap_servers="localhost:9092",
+        group_id="sender_main",
         enable_auto_commit=False,
     )
 
-    async for upload in consumer:
-        asyncio.create_task(
-            parse_uploads(
-                consumer,
-                upload
-            )
+    with db_engine.connect() as conn:
+        async with consumer:
+            async for upload in consumer:
+                asyncio.create_task(
+                    parse_uploads(conn, consumer, upload),
+                )
+
+
+async def parse_uploads(conn, main_consumer, upload_info):
+    pipelines_set = 0  # TODO get this from the upload_info record
+
+    consumer = AIOKafkaConsumer(
+        f"PIPELINES_DATA_READY_{pipelines_set}",
+        bootstrap_servers="localhost:9092",
+        group_id="sender_task_creator",
+    )
+
+    done_producer = AIOKafkaProducer(
+        bootstrap_servers="localhost:9092",
+    )
+
+    pipelines_done = set()
+    active_at_start = _get_active_pipelines(conn)
+
+    async with consumer:
+        while not _all_done(conn, active_at_start, pipelines_done):
+            done_notification = await consumer.getone()
+
+            send_updates.delay(done_notification)
+
+            pipelines_done.add(done_notification)
+
+    async with done_producer:
+        await asyncio.gather(
+            done_producer.send("PIPELINES_DONE", None, pipelines_set),
+            main_consumer.commit({TopicPartition(upload_info.topic, upload_info.partition): upload_info.offset}),
         )
 
 
-async def parse_uploads(main_consumer, upload_info):
-    consumer = AIOKafkaConsumer(
-        'my_topic', 'my_other_topic',
-        bootstrap_servers='localhost:9092',
-        group_id="my-group")
+def _all_done(conn, active_at_start, pipelines_done):
+    currently_active = _get_active_pipelines(conn)
 
-    # TODO get active pipelines
+    active_at_start.intersection_update(currently_active)
 
-    await consumer.start()
+    return len(set.intersection(active_at_start, pipelines_done)) == len(active_at_start)
 
-    count = 0
-    async for done_notification in consumer:
-        # TODO get active subscriptions subscriptions
 
-        send_updates.delay(done_notification)
+def _get_active_pipelines(conn):
+    results = await conn.execute('SELECT id FROM pipelines WHERE status = "ACTIVE"')
+    return set(row[0] for row in results)
 
-        pipelines_to_wait = 0  # TODO do the logic to check if all pipelines are done
-
-        count += 1
-        if count == pipelines_to_wait:
-            break
-
-    # TODO tell orchestrator is done
-
-    main_consumer.commit({TopicPartition(upload_info.topic, upload_info.partition): upload_info.offset})
-
-    await consumer.stop()
 
 asyncio.run(main())
