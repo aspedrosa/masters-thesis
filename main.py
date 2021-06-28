61a75b34d25adb0eaba63d8190bc037ecd0b7c17
aspedrosa <aspedrosa@ua.pt>
2021-06-28 20:40:34 +0100

    Add worker logic (A first working demo)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import os
 
 from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 from aiokafka.structs import TopicPartition
@@ -9,25 +10,40 @@ from kafka.errors import TopicAlreadyExistsError
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine
 
-from task import send_updates
+from task import launch_workers
 
 
 def json_deserializer(value):
     return json.loads(value.decode("utf-8"))
 
 
+def get_env_variable(name):
+    try:
+        return os.environ[name]
+    except KeyError:
+        raise ValueError(f"Missing {name} environment variable")
+
+
 async def main():
-    admin = KafkaAdminClient(bootstrap_servers=["localhost:29092"])
+    BOOTSTRAP_SERVERS = get_env_variable("BOOTSTRAP_SERVERS")
+    DB_HOST = get_env_variable("DB_HOST")
+    DB_PORT = get_env_variable("DB_PORT")
+    DB_USER = get_env_variable("DB_USER")
+    DB_PASSWORD = get_env_variable("DB_PASSWORD")
+    DB_NAME = get_env_variable("DB_NAME")
+    get_env_variable("DATA_READY_DIRECTORY")
+
+    admin = KafkaAdminClient(bootstrap_servers=BOOTSTRAP_SERVERS)
     try:
         admin.create_topics((NewTopic("PIPELINES_SETS_UPLOAD_NOTIFICATIONS", -1, -1, {}),))
     except TopicAlreadyExistsError:
         pass
 
-    db_engine = create_async_engine("sqlite+aiosqlite:///demo.db")
+    db_engine = create_async_engine(f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
     consumer = AIOKafkaConsumer(
         "PIPELINES_SETS_UPLOAD_NOTIFICATIONS",
-        bootstrap_servers="localhost:29092",
+        bootstrap_servers=BOOTSTRAP_SERVERS,
         group_id="sender_main",
         enable_auto_commit=False,
         value_deserializer=json_deserializer
@@ -41,17 +57,19 @@ async def main():
 
 
 async def parse_uploads(conn, main_consumer, upload_info):
+    BOOTSTRAP_SERVERS = os.getenv("BOOTSTRAP_SERVERS")
+
     pipelines_set = upload_info.value["pipelines_set"]
 
     consumer = AIOKafkaConsumer(
         f"PIPELINES_SET_{pipelines_set}_DATA_READY_TO_SEND",
-        bootstrap_servers="localhost:29092",
+        bootstrap_servers=BOOTSTRAP_SERVERS,
         group_id="sender_task_creator",
         value_deserializer=json_deserializer
     )
 
     done_producer = AIOKafkaProducer(
-        bootstrap_servers="localhost:29092",
+        bootstrap_servers=BOOTSTRAP_SERVERS,
     )
 
     pipelines_done = set()
@@ -63,9 +81,7 @@ async def parse_uploads(conn, main_consumer, upload_info):
 
             pipeline_id = done_notification.value["pipeline_id"]
 
-            # TODO for each subscription associated with this pipeline create a new task
-            #  or do all this on the celery task
-            send_updates.delay(upload_info.value["db_hash"], pipeline_id, done_notification.value["filename"])
+            launch_workers.delay(upload_info.value["db_hash"], pipeline_id, done_notification.value["filename"])
 
             pipelines_done.add(pipeline_id)
 
@@ -74,6 +90,8 @@ async def parse_uploads(conn, main_consumer, upload_info):
             done_producer.send("PIPELINES_SETS_DONE", pipelines_set.to_bytes(4, "big")),
             main_consumer.commit({TopicPartition(upload_info.topic, upload_info.partition): upload_info.offset}),
         )
+        # TODO there needs to be a more complex logic to check if I can commit a message, because I might be committing
+        #  previous messages and all their associated pipelines haven't finished yet
 
 
 async def _all_done(conn, active_at_start, pipelines_done):
@@ -85,7 +103,7 @@ async def _all_done(conn, active_at_start, pipelines_done):
 
 
 async def _get_active_pipelines(conn):
-    results = await conn.execute(text('SELECT id FROM pipelines WHERE status = "ACTIVE"'))
+    results = await conn.execute(text("SELECT id FROM pipelines WHERE status = 'ACTIVE'"))
     return set(row[0] for row in results)
 
 
--- task.py
+++ task.py
@@ -1,9 +1,112 @@
-from celery import Celery
+import ast
+import os
+import traceback
 
-app = Celery("sender", broker="redis://localhost")
+import pandas
+import requests
+from celery import Celery
+from jinja2 import Template
+from sqlalchemy import create_engine, text
+
+app = Celery("sender", broker="redis://localhost:6379")
+
+
+def create_db_engine():
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_NAME = os.getenv("DB_NAME")
+
+    return create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
 
 @app.task
-def send_updates(db_hash, pipeline_id, filename):
-    # TODO check if the associated subscription is active
-    pass
+def launch_workers(db_hash, pipeline_id, filename):
+    db_engine = create_db_engine()
+
+    with db_engine.connect() as conn:
+        for subscription in conn.execute(text(
+                'SELECT id from subscriptions '
+                f"WHERE status = 'ACTIVE' AND pipeline_id = {pipeline_id}"
+        )):
+            send_updates.delay(db_hash, filename, subscription[0])
+
+
+@app.task
+def send_updates(db_hash, filename, subscription_id):
+    DATA_READY_DIRECTORY = os.getenv("DATA_READY_DIRECTORY")
+
+    db_engine = create_db_engine()
+
+    with db_engine.connect() as conn:
+        results = conn.execute(text(
+            "SELECT 1 from subscriptions "
+            f"WHERE id = {subscription_id} AND status = 'ACTIVE'"
+        ))
+
+        try:
+            next(results)
+        except StopIteration:
+            return
+
+        subscription_requests = conn.execute(text(
+            "SELECT id, request_arguments_template, success_condition_template "
+            f"FROM requests WHERE subscription_id = {subscription_id} "
+            "ORDER BY request_order"
+        ))
+
+        with open(os.path.join(DATA_READY_DIRECTORY, filename)) as data_file, requests.Session() as session:
+
+            responses = []
+            context = {
+                "db_hash": db_hash,
+                "data_file": data_file,
+                "data": pandas.read_csv(data_file),
+                "responses": responses,
+            }
+
+            results = conn.execute(
+                "INSERT INTO subscription_logs (success_count, subscription_id) "
+                f"VALUES (0, {subscription_id}) "
+                "RETURNING id"
+            )
+            subscription_logs_id = results.fetchone()[0]
+
+            success_count = 0
+
+            for request_id, request_arguments_template, success_condition_template in subscription_requests:
+                try:
+                    render_result = Template(request_arguments_template).render(**context)
+
+                    response = session.request(**ast.literal_eval(render_result))  # TODO check if a dict was returned
+
+                    if success_condition_template is not None:
+                        render_result = Template(success_condition_template).render(response=response)
+
+                        if not bool(
+                                eval(  # noqa - we trust the admins
+                                    render_result
+                                )
+                        ):
+                            raise AssertionError("Success condition not met")
+                except:
+                    conn.execute(text(
+                        "INSERT INTO request_logs (request_id, subscription_log_id, success, exception) "
+                        f"VALUES ({request_id}, {subscription_logs_id}, false, '{traceback.format_exc()}')"
+                    ))
+                else:
+                    success_count += 1
+                    conn.execute(text(
+                        "INSERT INTO request_logs (request_id, subscription_log_id, success) "
+                        f"VALUES ({request_id}, {subscription_logs_id}, true)"
+                    ))
+
+                responses.append(response)
+
+            if success_count > 0:
+                conn.execute(text(
+                    "UPDATE subscription_logs "
+                    "SET success_count = {success_count} "
+                    f"WHERE id = {subscription_logs_id}"
+                ))
