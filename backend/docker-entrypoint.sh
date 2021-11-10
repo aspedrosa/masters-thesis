1d99eb0bf0c57971e7f6ba6440b58b72e0f8a1ef
aspedrosa <aspedrosa@ua.pt>
2021-11-10 11:36:42 +0000

    backend: install curl + add execution permissions to sh scripts

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 wait-for-it "$POSTGRES_HOST:$POSTGRES_PORT"
+wait-for-it "$KSQLDB_HOST:$KSQLDB_PORT"
+wait-for-it "$SCHEMA_REGISTRY_HOST:$SCHEMA_REGISTRY_PORT"
+
 gunicorn adminrest.wsgi:application --bind 0.0.0.0:8000 --workers 5
--- backend/docker-init.sh
+++ backend/docker-init.sh
@@ -7,6 +7,5 @@ wait-for-it "$POSTGRES_HOST:$POSTGRES_PORT"
 echo "Applying migrations"
 python manage.py migrate
 
-# Create an user for the admin app
 echo "Creating super user"
 python manage.py createsuperuser
