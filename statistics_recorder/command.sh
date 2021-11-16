fbcb785a1104e535957f810e7a993d8d84f8e7a8
aspedrosa <aspedrosa@ua.pt>
2021-11-16 12:20:15 +0000

    remove agent related code

--- statistics_recorder/command.sh
+++ statistics_recorder/command.sh
@@ -14,20 +14,7 @@ done
 
 nc -vz 0.0.0.0 8083
 
-echo -e "\n--\n+> Creating Kafka AchillesResult source Connect"
-
-CLEANUP_POLICY_CLASS=LocalMoveCleanupPolicy
-
-if ! [ -z "$AGENT_DELETE_CLEANUP_POLICY" ] ; then
-    value=$(echo $AGENT_DELETE_CLEANUP_POLICY | tr '[:upper:]' '[:lower:]')
-    VALID_YES_OPTIONS=("yes" "y" "true" "1")
-    for VALID_YES_OPTION in "${VALID_YES_OPTIONS[@]}" ; do
-        if [ $VALID_YES_OPTION = "$value" ] ; then
-            CLEANUP_POLICY_CLASS=DeleteCleanupPolicy
-            break
-        fi
-    done
-fi
+echo -e "\n--\n+> Creating Kafka sink Connectors"
 
 for variable in DB_HOST DB_PORT DB_DATABASE DB_USER DB_PASSWORD ; do
     sed -i "s/$variable/${!variable}/" /app/healthcheks.json
